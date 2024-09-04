f6e073d424c51cd57622c962a06c68f275066506
Admin <nikita.ishmekeev.2003@gmail.com>
2024-09-04 23:04:14 +0500

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите строку: ");
+        string input = Console.ReadLine();
+        int sum = 0;
+
+        foreach (char c in input)
+        {
+            if (char.IsDigit(c))
+            {
+                sum += int.Parse(c.ToString());
+            }
+        }
+        Console.WriteLine("Сумма всех цифр в строке: " + sum);
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("Введите строку: ");
+        string input = Console.ReadLine();
+
+        char[] delimiters = { ' ', '.', ',', ';', ':', '!', '?', '-', '_', '(', ')', '[', ']', '{', '}', '\"', '\'' };
+        string[] words = input.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+        int maxLength = words.Max(word => word.Length);
+
+        List<string> longestWords = words.Where(word => word.Length == maxLength).ToList();
+
+        Console.WriteLine("Самые длинные слова:");
+        foreach (string word in longestWords)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
